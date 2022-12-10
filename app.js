4e272711ccde9ced8fb0999b728087e2c067fb59
minesbus <minesbus@hotmail.com>
2022-12-10 18:54:47 -0300

    entrega de proyecto

---
+++ app.js
@@ -0,0 +1,85 @@
+//ejercicio3
+
+const persona  = {
+ id:0,  
+nombre:"ada lovelace",
+email: "ada@gmail.com",
+telefono:1234567890,
+}
+console.log("este es sus nombre:",persona.nombre)
+
+const persona1 = {
+   id:1,
+    nombre:"grace hopper ",
+    email: "grace@hotmail.com",
+    telefono:0987654321,
+}
+console.log("este es su id:",persona1.id)
+
+const persona2 = {
+id:2,
+nombre:"hedy lamar",
+email:"hedy@gmail.com",
+telefono:6789054321,
+}
+console.log("estes es su direccion de correo:" ,persona2.email)
+
+const persona3 = {
+   id:3,
+    nombre:"radia perlman",
+    email:"radia@yahoo.com",
+    telefono:1234509876,
+    }
+    console.log("este es su id:",persona3.id)
+    console.log( "este es su nombre:",persona3.nombre)
+   
+    
+    const persona4 = {
+        id:4,
+        nombre:"sherly sandeberg",
+        email:"sheryl@facebook.com",
+        telefono:5432167890,
+        }
+        console.log("este es su telefono",persona4.telefono)
+        
+   //ejercicio 4   
+
+var datos = {
+id:1,
+nombre: "ada",
+apellido: "lovelace",
+email: "ada.lovelace@gmail.com",
+telefono:"1234567890",
+edad:"29",
+programa: true,
+
+}
+datos.telefono = "0192837465";
+datos.edad = 29;
+console.log(datos);
+
+//ejercicio5
+
+
+
+var datos1 = {
+    
+    id: 1,
+    nombre: "Ada",
+   apellido: "Lovelace",
+    email: "ada.lovelace@gmail.com",
+    telefono: "1234567890",
+    edad: 29,
+    programa: true,
+    };
+
+    
+ if(datos1.programa === true); {
+datos1.lenguajefavorito = "javascript"; 
+     
+     
+}
+
+
+console.log(datos1);
+
